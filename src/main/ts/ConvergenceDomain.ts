d4527fd2f1efcfa84375dd9d3f632940f80444f7
Cameron Meyer <cameron@convergencelabs.com>
2016-01-19 11:03:36 -0800

    Added ConvergenceDomain Api

--- src/main/ts/ConvergenceDomain.ts
+++ src/main/ts/ConvergenceDomain.ts
@@ -1,5 +1,8 @@
 module convergence {
-  export class ConvergenceDomain {
+  import EventEmitter = convergence.util.EventEmitter;
+  import Session = convergence.Session;
+
+  export class ConvergenceDomain extends EventEmitter {
 
 
     /**
@@ -9,11 +12,58 @@ module convergence {
      *            The url of the convergence domain to connect to.
      */
     constructor(url: String) {
-
+      super();
     }
 
+    /**
+     * Authenticates the user with the given username and password.
+     * @param {string} username - The username of the user
+     * @param {string} password - The password of the user
+     * @return {Q.Promise} A promise
+     */
+    authenticateWithPassword(username, password): Q.Promise<void> {
+      return Q.resolve<void>(null);
+    }
+
+    /**
+     * Authenticates the user with the given username.
+     * @param {string} token - The identifier of the participant
+     * @return {Q.Promise} A promise
+     */
+    authenticateWithToken(token): Q.Promise<void> {
+      return Q.resolve<void>(null);
+    }
+
+    /**
+     * Gets the session of the local client.
+     * @return {convergence.Session} The local session.
+     */
+    getSession(): Session {
+      return null;
+    }
+
+    /**
+     * Gets the MessageService
+     * @returns {convergence.SharedModelService}
+     */
+    getSharedModelService(): void {
+      return null;
+    }
+
+    /**
+     * Closes the connection to the server and disposes of the ConvergenceDomain
+     */
     dispose(): void {
 
     }
+
+    /**
+     * @returns {boolean} True if this ConvergenceDomain is disposed.
+     */
+    isDisposed(): boolean {
+      return false;
+    }
+
+
   }
 }
