58f5ede823e255c87f6735c06bf9fc0175f0f2c9
Cameron <cameron@convergencelabs.com>
2016-02-02 20:52:15 -0800

    Updated tsconfig and add test ts file

---
+++ src/test/ts/ModelFqnSpec.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/chai/chai.d.ts" />
+
+import ModelFqn from "../../main/ts/model/ModelFqn";
+import * as chai from "chai"
+
+var expect = chai.expect;
+var fail = chai.assert.fail;
+
+
+describe('ModelFqn', function () {
+
+  it('Constructor set correct values', function () {
+    var collection = "collection";
+    var model = "model";
+
+    var fqn = new ModelFqn(collection, model);
+    expect(fqn.collectionId).to.equal(collection);
+    expect(fqn.modelId).to.equal(model);
+  });
+
+  it('Must be immutable', function () {
+    var fqn = new ModelFqn("collection", "model");
+
+    try {
+      fqn.collectionId = "foo";
+      fail();
+    } catch (e) {
+    }
+  });
+});
