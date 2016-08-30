f41f851f4ae2be72824cc18576131f9327dcbfe1
Cameron Meyer <cameron@convergencelabs.com>
2016-08-30 10:29:27 -0700

    Fixed element reference detached handling

--- src/main/ts/model/reference/ElementReference.ts
+++ src/main/ts/model/reference/ElementReference.ts
@@ -2,8 +2,13 @@ import {RealTimeValue} from "../rt/RealTimeValue";
 import {ModelReference} from "./ModelReference";
 import {ReferenceType} from "./ModelReference";
 import {RealTimeModel} from "../rt/RealTimeModel";
+import {ValueDetachedEvent} from "../observable/ObservableValue";
 
 export class ElementReference extends ModelReference<RealTimeValue<any>> {
+
+  private _detachedListener: Function = (event: ValueDetachedEvent) => {
+    this._handleElementRemoved(<RealTimeValue>event.src);
+  };
 
   constructor(key: string,
               source: RealTimeModel,
@@ -13,8 +18,27 @@ export class ElementReference extends ModelReference<RealTimeValue<any>> {
     super(ReferenceType.ELEMENT, key, source, username, sessionId, local);
   }
 
-  _handleElementRemoved(elemenet: RealTimeValue<any>): void {
-    var index: number = this._values.indexOf(elemenet, 0);
+  _set(values: RealTimeValue<any>[], local: boolean = false): void {
+    for (var oldElement of this.values()) {
+      oldElement.removeListener(RealTimeValue.Events.DETACHED, this._detachedListener);
+    }
+
+    // Add Detached Listeners
+    for (var newElement of values) {
+      newElement.addListener(RealTimeValue.Events.DETACHED, this._detachedListener);
+    }
+    super._set(values, local);
+  }
+
+  _clear(): void {
+    for (var oldElement of this.values()) {
+      oldElement.removeListener(RealTimeValue.Events.DETACHED, this._detachedListener);
+    }
+    super._clear();
+  }
+
+  _handleElementRemoved(element: RealTimeValue<any>): void {
+    var index: number = this._values.indexOf(element, 0);
     if (index > -1) {
       let newElements: RealTimeValue<any>[] = this._values.slice(0);
       newElements.splice(index, 1);
--- src/main/ts/model/reference/LocalElementReference.ts
+++ src/main/ts/model/reference/LocalElementReference.ts
@@ -5,32 +5,7 @@ import {ElementReference} from "./ElementReference";
 import {RealTimeValue} from "../rt/RealTimeValue";
 
 export class LocalElementReference extends LocalModelReference<RealTimeValue<any>, ElementReference> {
-
-  private _detachedListener: Function = (rtValue) => {
-    this._reference._handleElementRemoved(rtValue);
-  };
-
   constructor(reference: ElementReference, referenceCallbacks: ModelReferenceCallbacks, disposeCallback: ReferenceDisposedCallback) {
     super(reference, referenceCallbacks, disposeCallback);
-  }
-
-  set(value: RealTimeValue<any> | RealTimeValue<any>[]): void {
-    // Remove Old Detached Listeners
-    if (this.values() !== null) {
-      for (var oldElement of this.values()) {
-        oldElement.removeListener(RealTimeValue.Events.DETACHED, this._detachedListener);
-      }
-    }
-
-    // Add Detached Listeners
-    if (value instanceof Array) {
-      for (var newElement of value) {
-        newElement.addListener(RealTimeValue.Events.DETACHED, this._detachedListener);
-      }
-    } else {
-      (<RealTimeValue<any>> value).addListener(RealTimeValue.Events.DETACHED, this._detachedListener);
-    }
-
-    super.set(<RealTimeValue<any>[]> value);
   }
 }
