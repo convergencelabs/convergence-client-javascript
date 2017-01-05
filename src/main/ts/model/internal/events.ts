1b44f23156e1bcb5854fcdf1d7255714308d14b1
Cameron Meyer <cameron@convergencelabs.com>
2017-01-05 15:36:29 -0800

    internal model events updated to be readonly

--- src/main/ts/model/internal/events.ts
+++ src/main/ts/model/internal/events.ts
@@ -13,7 +13,8 @@ export interface ModelNodeEvent extends ConvergenceEvent {
 }
 
 export class NodeDetachedEvent implements ModelNodeEvent {
-  public name: string = "detached";
+  public static readonly NAME = "detached";
+  public name: string = NodeDetachedEvent.NAME;
 
   constructor(public src: ModelNode<any>,
               public local: boolean) {}
@@ -25,159 +26,203 @@ export interface NodeValueChangedEvent extends ModelNodeEvent {
 }
 
 export class NodeChangedEvent implements ModelNodeEvent {
+  public static readonly NAME = "node_changed";
+  public name: string = NodeChangedEvent.NAME;
 
-  public name: string = "node_changed";
-
-  constructor(public src: ModelNode<any>,
-              public local: boolean,
-              public relativePath: Path,
-              public childEvent: NodeValueChangedEvent,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ModelNode<any>,
+              public readonly local: boolean,
+              public readonly relativePath: Path,
+              public readonly childEvent: NodeValueChangedEvent,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ArrayNodeInsertEvent implements NodeValueChangedEvent {
-  public name: string = "insert";
+  public static readonly NAME = "insert";
+  public name: string = ArrayNodeInsertEvent.NAME;
 
-  constructor(public src: ArrayNode,
-              public local: boolean,
-              public index: number,
-              public value: ModelNode<any>,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ArrayNode,
+              public readonly local: boolean,
+              public readonly index: number,
+              public readonly value: ModelNode<any>,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ArrayNodeRemoveEvent implements NodeValueChangedEvent {
-  public name: string = "remove";
+  public static readonly NAME = "remove";
+  public name: string = ArrayNodeRemoveEvent.NAME;
 
-  constructor(public src: ArrayNode,
-              public local: boolean,
-              public index: number,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ArrayNode,
+              public readonly local: boolean,
+              public readonly index: number,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ArrayNodeSetEvent implements NodeValueChangedEvent {
-  public name: string = "set";
+  public static readonly NAME = "set";
+  public name: string = ArrayNodeSetEvent.NAME;
 
-  constructor(public src: ArrayNode,
-              public local: boolean,
-              public index: number,
-              public value: ModelNode<any>,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ArrayNode,
+              public readonly local: boolean,
+              public readonly index: number,
+              public readonly value: ModelNode<any>,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ArrayNodeReorderEvent implements NodeValueChangedEvent {
-  public name: string = "reorder";
+  public static readonly NAME = "reorder";
+  public name: string = ArrayNodeReorderEvent.NAME;
 
-  constructor(public src: ArrayNode,
-              public local: boolean,
-              public fromIndex: number,
-              public toIndex: number,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ArrayNode,
+              public readonly local: boolean,
+              public readonly fromIndex: number,
+              public readonly toIndex: number,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ArrayNodeSetValueEvent implements NodeValueChangedEvent {
-  public name: string = "value";
+  public static readonly NAME = "value";
+  public name: string = ArrayNodeSetValueEvent.NAME;
 
-  constructor(public src: ArrayNode,
-              public local: boolean,
-              public value: any[],
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ArrayNode,
+              public readonly local: boolean,
+              public readonly value: any[],
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class BooleanNodeSetValueEvent implements NodeValueChangedEvent {
-  public name: string = "value";
+  public static readonly NAME = "value";
+  public name: string = BooleanNodeSetValueEvent.NAME;
 
-  constructor(public src: BooleanNode,
-              public local: boolean,
-              public value: boolean,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: BooleanNode,
+              public readonly local: boolean,
+              public readonly value: boolean,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class NumberNodeSetValueEvent implements NodeValueChangedEvent {
-  public name: string = "value";
+  public static readonly NAME = "value";
+  public name: string = NumberNodeSetValueEvent.NAME;
 
-  constructor(public src: NumberNode,
-              public local: boolean,
-              public value: number,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: NumberNode,
+              public readonly local: boolean,
+              public readonly value: number,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class NumberNodeDeltaEvent implements NodeValueChangedEvent {
-  public name: string = "delta";
+  public static readonly NAME = "delta";
+  public name: string = NumberNodeDeltaEvent.NAME;
 
-  constructor(public src: NumberNode,
-              public local: boolean,
-              public value: number,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: NumberNode,
+              public readonly local: boolean,
+              public readonly value: number,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ObjectNodeSetEvent implements NodeValueChangedEvent {
-  public name: string = "set";
+  public static readonly NAME = "set";
+  public name: string = ObjectNodeSetEvent.NAME;
 
-  constructor(public src: ObjectNode,
-              public local: boolean,
-              public key: string,
-              public value: ModelNode<any>,
-              public sessionId: string,
-              public username: string) {}
+  constructor(public readonly src: ObjectNode,
+              public readonly local: boolean,
+              public readonly key: string,
+              public readonly value: ModelNode<any>,
+              public readonly sessionId: string,
+              public readonly username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ObjectNodeRemoveEvent implements NodeValueChangedEvent {
-  public name: string = "remove";
+  public static readonly NAME = "remove";
+  public name: string = ObjectNodeRemoveEvent.NAME;
 
   constructor(public src: ObjectNode,
               public local: boolean,
               public key: string,
               public sessionId: string,
-              public username: string) {}
+              public username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class ObjectNodeSetValueEvent implements NodeValueChangedEvent {
-  public name: string = "value";
+  public static readonly NAME = "value";
+  public name: string = ObjectNodeSetValueEvent.NAME;
 
   constructor(public src: ObjectNode,
               public local: boolean,
               public value: { [key: string]: any; },
               public sessionId: string,
-              public username: string) {}
+              public username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class StringNodeInsertEvent implements NodeValueChangedEvent {
-  public name: string = "insert";
+  public static readonly NAME = "insert";
+  public name: string = StringNodeInsertEvent.NAME;
 
   constructor(public src: StringNode,
               public local: boolean,
               public index: number,
               public value: string,
               public sessionId: string,
-              public username: string) {}
+              public username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class StringNodeRemoveEvent implements NodeValueChangedEvent {
-  public name: string = "remove";
+  public static readonly NAME = "remove";
+  public name: string = StringNodeRemoveEvent.NAME;
 
   constructor(public src: StringNode,
               public local: boolean,
               public index: number,
               public value: string,
               public sessionId: string,
-              public username: string) {}
+              public username: string) {
+    Object.freeze(this);
+  }
 }
 
 export class StringNodeSetValueEvent implements NodeValueChangedEvent {
-  public name: string = "value";
+  public static readonly NAME = "value";
+  public name: string = StringNodeSetValueEvent.NAME;
 
   constructor(public src: StringNode,
               public local: boolean,
               public value: string,
               public sessionId: string,
-              public username: string) {}
+              public username: string) {
+    Object.freeze(this);
+  }
 }
