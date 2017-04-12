a14715f778adc851352f183fe1c57617dab449cb
Michael MacFadden <michael@convergencelabs.com>
2017-04-12 15:50:40 -0500

    Fixed a lint error.

--- api/model/ModelPermissionManager.d.ts
+++ api/model/ModelPermissionManager.d.ts
@@ -8,19 +8,23 @@ export declare class ModelPermissionManager {
   public getPermissions(): Promise<ModelPermissions>;
 
   public getOverridesCollection(): Promise<boolean>;
+
   public setOverridesCollection(overridesCollection: boolean): Promise<void>;
 
   public getWorldPermissions(): Promise<ModelPermissions>;
+
   public setWorldPermissions(permissions: ModelPermissions): Promise<void>;
 
   public getAllUserPermissions(): Promise<Map<string, ModelPermissions>>;
-  
+
   public setAllUserPermissions(permissions: {[key: string]: ModelPermissions}): Promise<void>
   public setAllUserPermissions(permissions: Map<string, ModelPermissions>): Promise<void>
   public setAllUserPermissions(
     permissions: Map<string, ModelPermissions> | {[key: string]: ModelPermissions}): Promise<void>;
 
   public getUserPermissions(username: string): Promise<ModelPermissions>;
+
   public setUserPermissions(username: string, permissions: ModelPermissions): Promise<void>;
+
   public removeUserPermissions(username: string): Promise<void>;
 }
