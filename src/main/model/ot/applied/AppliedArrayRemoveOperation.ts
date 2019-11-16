09c5d37acdfa5b87f8a4dbae0444c05f1e6178d2
Michael MacFadden <michael@convergencelabs.com>
2019-11-16 12:26:40 -0600

    Updated the copyright header.

--- src/main/model/ot/applied/AppliedArrayRemoveOperation.ts
+++ src/main/model/ot/applied/AppliedArrayRemoveOperation.ts
@@ -1,8 +1,15 @@
 /*
  * Copyright (c) 2019 - Convergence Labs, Inc.
  *
- * This file is subject to the terms and conditions defined in the files
- * 'LICENSE' and 'COPYING.LESSER', which are part of this source code package.
+ * This file is part of the Convergence JavaScript Client, which is released
+ * under the terms of the GNU Lesser General Public License version 3
+ * (LGPLv3), which is a refinement of the GNU Lesser General Public License
+ * version 3 (GPLv3).  A copy of the both the GPLv3 and the LGPLv3 should have
+ * been provided along with this file, typically located in the "LICENSE" and
+ * "LICENSE.LGPL" files (respectively), which are part of this source code
+ * package. Alternatively, see <https://www.gnu.org/licenses/gpl-3.0.html> and
+ * <https://www.gnu.org/licenses/lgpl-3.0.html> for the full text of the GPLv3
+ * and LGPLv3 licenses, if they were not provided.
  */
 
 import {DataValue} from "../../dataValue";
