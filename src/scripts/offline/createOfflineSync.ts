b8406caffe4526b5dfa85588872ab9974634397a
Alec LaLonde <alec@boxelderweb.com>
2019-12-04 12:37:16 -0700

    Added manifest model to offline sync script

--- src/scripts/offline/createOfflineSync.ts
+++ src/scripts/offline/createOfflineSync.ts
@@ -3,6 +3,7 @@
 import {createDomain} from "../connect";
 import { IdbStorageAdapter } from "../../main/storage/idb/";
 import { OfflineModelSyncCompletedEvent } from "../../main/model/events/";
+import { RealTimeModel } from "../../main";
 
 // tslint:disable-next-line
 require("fake-indexeddb/auto");
@@ -17,9 +18,11 @@ const domain = createDomain({
 });
 
 async function createModels(): Promise<void> {
+  let rtManifest = await createManifest();
   for (let i = 0; i < MODELS_TO_CREATE; i++) {
     let id = `created-offline-${i}`;
     await createModel(id);
+    rtManifest.root().set(id, new Date().getTime());
     modelIds.push(id);
     console.log("Created offline model", id);
   }
@@ -36,11 +39,21 @@ async function createModel(id: string): Promise<string> {
   return model.modelId();
 }
 
+function createManifest(): Promise<RealTimeModel> {
+  let modelService = domain.models();
+
+  return modelService.openAutoCreate({
+    id: "manifest",
+    collection: "test"
+  });
+}
+
 async function cleanupModels(): Promise<void> {
   for (let id of modelIds) {
     console.log("deleting", id);
     await domain.models().remove(id).catch(e => console.error(e));
   }
+  await domain.models().remove("manifest").catch(e => console.error(e));
 }
 
 function go() {
@@ -48,11 +61,11 @@ function go() {
     .then(createModels)
     .then(() => {
       console.log("going online");
-      // domain.models().on(OfflineModelSyncCompletedEvent.NAME, async () => {
-      //   console.log("model sync completed");
-      //   await cleanupModels();
-      //   process.exit();
-      // });
+      domain.models().on(OfflineModelSyncCompletedEvent.NAME, async () => {
+        console.log("model sync completed");
+        await cleanupModels();
+        process.exit();
+      });
       return domain.connectWithPassword({username: "test", password: "password"});
     })
     .then(async () => {
