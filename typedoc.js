6911fc05474ec915aa38ff01f12ed0ffd4d3e7fb
Michael MacFadden <macfadden.michael@solute.us>
2021-04-10 16:57:25 -0500

    Starting to fix type doc errors.

--- typedoc.js
+++ typedoc.js
@@ -1,27 +1,24 @@
 module.exports = {
   name: "Convergence JavaScript Client API Documentation",
-  target: "ES6",
 
   excludeExternals: true,
-  excludeNotExported: true,
+
   excludePrivate: true,
   excludeProtected: true,
   exclude: [
     '**/model/ot/**',
     '**/model/internal/**',
-    '**/model/rt/richtext/**',
-    '**/index' // this doesn't do anything..
+    '**/model/rt/richtext/**'
   ],
 
-  mode: "modules",
   out: "dist/docs",
-  baseUrl: "src/main/ts",
+  entryPoints: ["src/main/ts/index.ts"],
   readme: "src/docs/README.md",
   json: "dist/docs.json",
 
   theme: "node_modules/@convergencelabs/typedoc-theme/bin/default",
 
-  gaId: "UA-84372428-3",
+  gaID: "UA-84372428-3",
 
   hideGenerator: true
 };
