3a21e90fe5f5757be3b4fad2aec6d9d61a344f2d
Michael MacFadden <macfadden.michael@solute.us>
2021-04-26 11:33:02 -0500

    Downgraded typedoc so our theme works again.
    This will be reverted when we upgrade.

--- typedoc.js
+++ typedoc.js
@@ -1,24 +1,27 @@
 module.exports = {
   name: "Convergence JavaScript Client API Documentation",
+  target: "ES6",
 
   excludeExternals: true,
-
+  excludeNotExported: true,
   excludePrivate: true,
   excludeProtected: true,
   exclude: [
     '**/model/ot/**',
     '**/model/internal/**',
-    '**/model/rt/richtext/**'
+    '**/model/rt/richtext/**',
+    '**/index' // this doesn't do anything..
   ],
 
+  mode: "modules",
   out: "dist/docs",
-  entryPoints: ["src/main/ts/index.ts"],
+  baseUrl: "src/main/ts",
   readme: "src/docs/README.md",
   json: "dist/docs.json",
 
   theme: "node_modules/@convergencelabs/typedoc-theme/bin/default",
 
-  gaID: "UA-84372428-3",
+  gaId: "UA-84372428-3",
 
   hideGenerator: true
 };
